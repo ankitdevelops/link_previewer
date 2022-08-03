ccc6471c48a931daa03f8c8d2c1e00f4b14711d1
Ankit Kumar <heyankitkr@gmail.com>
2022-08-04 00:35:03 +0530

    deploy changes

--- link_preview/settings.py
+++ link_preview/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0takl1ve=-s-pl@f5g43y!8so4^=47rqt6(i@=!zcs#)=_7%*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://linkdetails.herokuapp.com/']
+ALLOWED_HOSTS = ['linkdetails.herokuapp.com']
 
 
 # Application definition
