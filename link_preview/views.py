0da3e543813741dcc4a4b4d5419c104149ae872f
Ankit Kumar <heyankitkr@gmail.com>
2022-08-04 08:30:22 +0530

    few fixes

--- link_preview/views.py
+++ link_preview/views.py
@@ -10,12 +10,11 @@ def home(request):
     if url:
         response = requests.get(url)
         html_document = BeautifulSoup(response.content, "html.parser")
-
         context = {
             "title": get_title(html_document),
             "description": get_description(html_document),
             "image": get_image(html_document),
-            "domain": get_domain(html_document),
+            "domain": get_domain(html_document,url),
         }
     else:
         context = None
@@ -24,11 +23,11 @@ def home(request):
 
 def get_title(html_document):
     title = None
-    if not title:
+    if html_document.find("meta", property="og:title") :
         title = html_document.find("meta", property="og:title").get("content")
-    elif not title:
+    elif html_document.find("meta", property="twitter:title"):
         title = html_document.find("meta", property="twitter:title").get("content")
-    elif not title:
+    elif html_document.title:
         title = html_document.title.string
     else:
         title = html_document.find("h1").string
@@ -37,11 +36,11 @@ def get_title(html_document):
 
 def get_description(html_document):
     description = None
-    if not description:
+    if  html_document.find("meta", property="og:description"):
         description = html_document.find("meta", property="og:description").get(
             "content"
         )
-    elif not description:
+    elif html_document.find("meta", property="twitter:description"):
         description = html_document.find("meta", property="twitter:description").get(
             "content"
         )
@@ -52,22 +51,24 @@ def get_description(html_document):
 
 def get_image(html_document):
     image = None
-    if not image:
+    if  html_document.find("meta", property="og:image"):
         image = html_document.find("meta", property="og:image").get("content")
-    else:
+    elif html_document.find_all("img"):
         url = html_document.find_all("img")
         for i in url:
             if i.get("src").startswith("https"):
                 image = i.get("src")
                 break
-
+        image = "https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg"
     return image
 
 
-def get_domain(html_document):
+def get_domain(html_document,url):
     domain = None
-    if not domain:
+    if html_document.find("meta", property="og:url"):
         domain = html_document.find("meta", property="og:url").get("content")
+    elif html_document.find("link", rel="canonical"):
+         domain = html_document.find("link", rel="canonical").get("href")
     else:
-        domain = html_document.find("link", rel="canonical").get("href")
+       domain = url
     return domain
